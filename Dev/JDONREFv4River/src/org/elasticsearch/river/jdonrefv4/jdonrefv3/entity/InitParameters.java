362f20c9439f26f75398dc8e69abe4f17ba54d9f
najimou <najimou@c5994a56-1a20-43aa-ae4d-37d34f6eefe1>
2014-12-12 21:20:08 +0000

    rename

--- Dev/JDONREFv4River/src/org/elasticsearch/river/jdonrefv4/jdonrefv3/entity/InitParameters.java
+++ Dev/JDONREFv4River/src/org/elasticsearch/river/jdonrefv4/jdonrefv3/entity/InitParameters.java
@@ -19,7 +19,7 @@ import org.elasticsearch.river.jdonrefv4.jdonrefv3.dao.CommuneDAO;
  *
  * @author akchana
  */
-public class Parameters {
+public class InitParameters {
     
      String[] listeDepartement;
      String[] listeDepartementN;
@@ -30,11 +30,11 @@ public class Parameters {
     /**
      * 
      */
-    protected static Parameters instance = null;
-    public static Parameters getInstance() throws SQLException
+    protected static InitParameters instance = null;
+    public static InitParameters getInstance() throws SQLException
     {
         if (instance==null){
-            instance = new Parameters();
+            instance = new InitParameters();
         }
         return instance;
     }
@@ -95,7 +95,7 @@ public class Parameters {
                 allDept.add(c.dpt_code_departement);
             }
         } catch (SQLException ex) {
-            Logger.getLogger(Parameters.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(InitParameters.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
@@ -237,135 +237,135 @@ public class Parameters {
        
        
        
-        public static void main(String[] args) {
-            String[] IDF = {"95", "94", "93", "92", "91", "78", "77", "75"};
-            String[] departements = {"95", "75","85", "75", "75", "75","95"};
-            
-            for(int i=0;i<departements.length;i++){ 
-                System.out.println(departements[i]);
-            }
-            System.out.println("retrait des doublons :");
-            String[] tmp = deleteDoublon(departements);
-            for(int i=0;i<tmp.length;i++){ 
-                System.out.println(tmp[i]);
-            }
-            
-             ArrayList<String> arg2 = new ArrayList<>();
-            String tmp121 = "init3";    
-            String tmp122 = "init3";    
-            String tmp123 = "init3";    
-            String tmp13 = "15";    
-            String tmp141 = "19";    
-            String tmp142 = "19";    
-            String tmp143 = "19";    
-            String tmp15 = "66";           
-            String tmp16 = "30";  
-            String tmp171 = "41";  
-            String tmp172 = "41";  
-            String tmp173 = "41";  
-            arg2.add(tmp121);
-            arg2.add(tmp122);
-            arg2.add(tmp123);
-            arg2.add(tmp13);
-            arg2.add(tmp141);
-            arg2.add(tmp142);
-            arg2.add(tmp143);
-            arg2.add(tmp15);
-            arg2.add(tmp16);
-            arg2.add(tmp171);
-            arg2.add(tmp172);
-            arg2.add(tmp173);
-            deleteDoublon(arg2);
-            
-        String connectionString = "jdbc:postgresql://localhost:5432/JDONREF_IGN2";
-        String user = "postgres";
-        String passwd = "postgres";
-        Connection connection = null;
-        Parameters p = null;
-        try {
-            connection = DriverManager.getConnection(connectionString,user,passwd);
-            p = Parameters.getInstance();
-            p.allDeptInit(connection);
-        } catch (SQLException ex) {
-            Logger.getLogger(Parameters.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-            p.init1();
-            String[] str1 = p.getListeDepartement();
-            String[] strN1 = p.getListeDepartementN();
-             System.out.println("dept complet = "+str1.length);
-             System.out.println("deptN 1 = "+strN1.length);
-
-            p.init2(departements);
-            String[] str2 = p.getListeDepartement();
-            String[] strN2 = p.getListeDepartementN();
-             System.out.println("prends en parametre liste departements et enleve les doublons) = "+str2.length);
-             System.out.println("deptN 2= "+strN2.length);
-             
-             //            p.init3("99","75");
-            p.init3("95","95");
-            String[] str3 = p.getListeDepartement();
-            String[] strN3 = p.getListeDepartementN();
-             System.out.println("intervalle departemet = "+str3.length);
-             System.out.println("deptN 3 = "+strN3.length);
-             
-            
-            p.init2(IDF);
-            String[] str4 = p.getListeDepartement();
-             System.out.println("IDF = "+str4.length);
-
-             ArrayList<String> str = new ArrayList(Arrays.asList(str3));
-             str.addAll(Arrays.asList(str4));
-             for(String s : str){
-                 System.out.println(s);
-             }
-             System.out.println("before (avec doublons)= "+str.size());
-             deleteDoublon(str);
-             for(String s : str){
-                 System.out.println(s);
-             }
-             
-
-             System.out.println("after (sans doublons)= "+str.size());
-             String[] str5 = (String[])str.toArray(new String[str.size()]);
-             int taille5 = str5.length;
-             System.out.println("taille5 = "+taille5);
-             
-            for(int i=0;i<str3.length;i++){ 
-                System.out.println(str3[i]);
-            }
-            System.out.println("taille = "+str3.length);
-//            for(int i=0;i<strN3.length;i++){ 
-//                System.out.println(strN3[i]);
+//        public static void main(String[] args) {
+//            String[] IDF = {"95", "94", "93", "92", "91", "78", "77", "75"};
+//            String[] departements = {"95", "75","85", "75", "75", "75","95"};
+//            
+//            for(int i=0;i<departements.length;i++){ 
+//                System.out.println(departements[i]);
 //            }
-//            System.out.println("taille = "+strN3.length);
-
-            ArrayList<String[]> arg = new ArrayList<>();
-            String[] tmp2 = {"init3","01","10"};    //10
-            String[] tmp3 = {"init3","15","05"};    //11 ==> 10+5=15   
-            String[] tmp4 = {"init3","19","21"};    //4 ==> 15+4 = 19
-            String[] tmp5 = {"66","67"};            //2 ==> 19+2 = 21    
-            String[] tmp6 = {"30","31","32","38"};  //4 ==> 21+4 = 25
-            String[] tmp7 = {"41","45","57","70"};  //4 ==> 25+4 = 29
-
-            arg.add(IDF);
-            arg.add(tmp2);
-            arg.add(tmp3);
-            arg.add(tmp4);
-            arg.add(tmp5);
-            arg.add(tmp6);
-            arg.add(tmp7);
-            arg.add(tmp7);
-            p.init4(arg);
-            String[] str7 = p.getListeDepartement();
-            String[] strN7 = p.getListeDepartementN();
-             System.out.println("dept liste = "+str7.length);
-             System.out.println("deptN 7 = "+strN7.length);
-             for(int i=0;i<str7.length;i++){ 
-                System.out.println(str7[i]);
-            }
-            
-            
-    }
+//            System.out.println("retrait des doublons :");
+//            String[] tmp = deleteDoublon(departements);
+//            for(int i=0;i<tmp.length;i++){ 
+//                System.out.println(tmp[i]);
+//            }
+//            
+//             ArrayList<String> arg2 = new ArrayList<>();
+//            String tmp121 = "init3";    
+//            String tmp122 = "init3";    
+//            String tmp123 = "init3";    
+//            String tmp13 = "15";    
+//            String tmp141 = "19";    
+//            String tmp142 = "19";    
+//            String tmp143 = "19";    
+//            String tmp15 = "66";           
+//            String tmp16 = "30";  
+//            String tmp171 = "41";  
+//            String tmp172 = "41";  
+//            String tmp173 = "41";  
+//            arg2.add(tmp121);
+//            arg2.add(tmp122);
+//            arg2.add(tmp123);
+//            arg2.add(tmp13);
+//            arg2.add(tmp141);
+//            arg2.add(tmp142);
+//            arg2.add(tmp143);
+//            arg2.add(tmp15);
+//            arg2.add(tmp16);
+//            arg2.add(tmp171);
+//            arg2.add(tmp172);
+//            arg2.add(tmp173);
+//            deleteDoublon(arg2);
+//            
+//        String connectionString = "jdbc:postgresql://localhost:5432/JDONREF_IGN2";
+//        String user = "postgres";
+//        String passwd = "postgres";
+//        Connection connection = null;
+//        InitParameters p = null;
+//        try {
+//            connection = DriverManager.getConnection(connectionString,user,passwd);
+//            p = InitParameters.getInstance();
+//            p.allDeptInit(connection);
+//        } catch (SQLException ex) {
+//            Logger.getLogger(InitParameters.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//            p.init1();
+//            String[] str1 = p.getListeDepartement();
+//            String[] strN1 = p.getListeDepartementN();
+//             System.out.println("dept complet = "+str1.length);
+//             System.out.println("deptN 1 = "+strN1.length);
+//
+//            p.init2(departements);
+//            String[] str2 = p.getListeDepartement();
+//            String[] strN2 = p.getListeDepartementN();
+//             System.out.println("prends en parametre liste departements et enleve les doublons) = "+str2.length);
+//             System.out.println("deptN 2= "+strN2.length);
+//             
+//             //            p.init3("99","75");
+//            p.init3("95","95");
+//            String[] str3 = p.getListeDepartement();
+//            String[] strN3 = p.getListeDepartementN();
+//             System.out.println("intervalle departemet = "+str3.length);
+//             System.out.println("deptN 3 = "+strN3.length);
+//             
+//            
+//            p.init2(IDF);
+//            String[] str4 = p.getListeDepartement();
+//             System.out.println("IDF = "+str4.length);
+//
+//             ArrayList<String> str = new ArrayList(Arrays.asList(str3));
+//             str.addAll(Arrays.asList(str4));
+//             for(String s : str){
+//                 System.out.println(s);
+//             }
+//             System.out.println("before (avec doublons)= "+str.size());
+//             deleteDoublon(str);
+//             for(String s : str){
+//                 System.out.println(s);
+//             }
+//             
+//
+//             System.out.println("after (sans doublons)= "+str.size());
+//             String[] str5 = (String[])str.toArray(new String[str.size()]);
+//             int taille5 = str5.length;
+//             System.out.println("taille5 = "+taille5);
+//             
+//            for(int i=0;i<str3.length;i++){ 
+//                System.out.println(str3[i]);
+//            }
+//            System.out.println("taille = "+str3.length);
+////            for(int i=0;i<strN3.length;i++){ 
+////                System.out.println(strN3[i]);
+////            }
+////            System.out.println("taille = "+strN3.length);
+//
+//            ArrayList<String[]> arg = new ArrayList<>();
+//            String[] tmp2 = {"init3","01","10"};    //10
+//            String[] tmp3 = {"init3","15","05"};    //11 ==> 10+5=15   
+//            String[] tmp4 = {"init3","19","21"};    //4 ==> 15+4 = 19
+//            String[] tmp5 = {"66","67"};            //2 ==> 19+2 = 21    
+//            String[] tmp6 = {"30","31","32","38"};  //4 ==> 21+4 = 25
+//            String[] tmp7 = {"41","45","57","70"};  //4 ==> 25+4 = 29
+//
+//            arg.add(IDF);
+//            arg.add(tmp2);
+//            arg.add(tmp3);
+//            arg.add(tmp4);
+//            arg.add(tmp5);
+//            arg.add(tmp6);
+//            arg.add(tmp7);
+//            arg.add(tmp7);
+//            p.init4(arg);
+//            String[] str7 = p.getListeDepartement();
+//            String[] strN7 = p.getListeDepartementN();
+//             System.out.println("dept liste = "+str7.length);
+//             System.out.println("deptN 7 = "+strN7.length);
+//             for(int i=0;i<str7.length;i++){ 
+//                System.out.println(str7[i]);
+//            }
+//            
+//            
+//    }
     
 }
